fb5eb0bd58a8767e1f553f397fd4239b12c1eeb4
Qdy225875 <2116375685@qq.com>
2024-10-05 00:39:16 +0800

    Add failing test

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -1,5 +1,8 @@
 package org.example;
 
+
+import java.util.Scanner;
+
 public class Calc {
 
     public int add(int x, int y)
@@ -10,5 +13,9 @@ public class Calc {
     public int subtract(int x, int y)
     {
         return x*y;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
     }
 }
--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -7,8 +7,14 @@ public class CalcTest {
   Calc c = new Calc();
 
     @Test
-    void testAddition() {
-        assertEquals(4, c.add(2,2));
+    public void testAddition() {
+        assertEquals(4, c.add(2, 2));
     }
+
+    @Test
+    public void testSubtraction() {
+        assertEquals(2, c.subtract(4, 2));
+    }
+
 
 }
